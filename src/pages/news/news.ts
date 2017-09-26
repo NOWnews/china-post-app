34002f788238ba1ba789c21039dbca3aa79ab7c5
Webber <bird23074035@gmail.com>
2017-09-26 15:28:25 +0800

    🎨 Style: 加上 nav, 加上 slide

--- src/pages/news/news.ts
+++ src/pages/news/news.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
-import { NavController } from 'ionic-angular';
+import { NavController, Slides } from 'ionic-angular';
 
 import { NewsData } from '../../providers/news-data';
+import { CategoriesData } from '../../providers/categories-data';
 import { OneDetailPage } from '../one-detail/one-detail';
 
 @Component({
@@ -10,16 +11,44 @@ import { OneDetailPage } from '../one-detail/one-detail';
   templateUrl: 'news.html'
 })
 export class NewsPage {
+  @ViewChild('navbar') slider: Slides;
+
   categories: any;
+  categoriesList: any;
+  catSlides: any;
 
   constructor(
     public navCtrl: NavController,
-    public catData: NewsData
-  ) {}
+    public catData: NewsData,
+    public catListData: CategoriesData
+  ) { }
+
+  onSlideWillChange(s: Slides) {
+    console.log(`onSlideWillChange: ${s}`);
+  }
+
+  onSlideDidChange(s: Slides) {
+    console.log(`onSlideDidChange: ${s}`);
+  }
+
+  onSlideDrag(s: Slides) {
+    if (!this.slider.freeMode){
+      this.slider.freeMode = true;
+    }
+    console.log(`onSlideDrag: ${s}`);
+  }
 
   ionViewDidLoad() {
     this.catData.getCategories().subscribe((categories: any[]) => {
+      categories.map((news: any)=>{
+        news.image = news.image || 'http://www.chinapost.com.tw/news_images/20170920/viewpoint.jpg';
+        return news;
+      })
       this.categories = categories;
+      this.catSlides = categories.slice(0,5);
+    });
+    this.catListData.getCategoriesList().subscribe((categories: any[]) => {
+      this.categoriesList = categories;
     });
   }
 
