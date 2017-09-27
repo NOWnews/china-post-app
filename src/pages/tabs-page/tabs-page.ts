19dc75352decd26e8701bd89da0618cef84256ed
Webber <bird23074035@gmail.com>
2017-09-27 14:28:23 +0800

    🔨 Refactor: 用其淫巧既暫時處理一下

--- src/pages/tabs-page/tabs-page.ts
+++ src/pages/tabs-page/tabs-page.ts
@@ -6,6 +6,13 @@ import { NewsPage } from '../news/news';
 import { TaiwanPage } from '../taiwan/news';
 import { WorldPage } from '../world/news';
 import { LifePage } from '../life/news';
+import { AsiaPage } from '../asia/news';
+import { BusinessPage } from '../business/news';
+import { SportsPage } from '../sports/news';
+import { TravelPage } from '../travel/news';
+import { ArtPage } from '../art/news';
+import { EditorialPage } from '../editorial/news';
+import { HightlightPage } from '../hightlight/news';
 
 @Component({
   templateUrl: 'tabs-page.html'
@@ -16,6 +23,13 @@ export class TabsPage {
   tab2Root: any = TaiwanPage;
   tab3Root: any = WorldPage;
   tab4Root: any = LifePage;
+  tab5Root: any = AsiaPage;
+  tab6Root: any = BusinessPage;
+  tab7Root: any = SportsPage;
+  tab8Root: any = TravelPage;
+  tab9Root: any = ArtPage;
+  tab10Root: any = EditorialPage;
+  tab11Root: any = HightlightPage;
   mySelectedIndex: number;
 
   constructor(navParams: NavParams) {
