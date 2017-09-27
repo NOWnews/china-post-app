9d01996573408c936ad40b34662ea9a43ddcff18
Webber <bird23074035@gmail.com>
2017-09-27 16:39:58 +0800

    ✨ Feat: 加上上垃跟下拉的動態

--- src/pages/news/news.ts
+++ src/pages/news/news.ts
@@ -3,7 +3,7 @@ import { Component, ViewChild } from '@angular/core';
 import { NavController, Slides } from 'ionic-angular';
 
 import { NewsData } from '../../providers/news-data';
-import { CategoriesData } from '../../providers/categories-data';
+// import { CategoriesData } from '../../providers/categories-data';
 import { OneDetailPage } from '../one-detail/one-detail';
 
 @Component({
@@ -14,7 +14,7 @@ export class NewsPage {
   @ViewChild('navbar') slider: Slides;
 
   categories: any;
-  categoriesList: any;
+  // categoriesList: any;
   catSlides: any;
   hasNews: boolean;
   catId: number = 1;
@@ -22,23 +22,23 @@ export class NewsPage {
   constructor(
     public navCtrl: NavController,
     public catData: NewsData,
-    public catListData: CategoriesData
+    // public catListData: CategoriesData
   ) { }
 
-  onSlideWillChange(s: Slides) {
-    console.log(`onSlideWillChange: ${s}`);
-  }
+  // onSlideWillChange(s: Slides) {
+  //   console.log(`onSlideWillChange: ${s}`);
+  // }
 
-  onSlideDidChange(s: Slides) {
-    console.log(`onSlideDidChange: ${s}`);
-  }
+  // onSlideDidChange(s: Slides) {
+  //   console.log(`onSlideDidChange: ${s}`);
+  // }
 
-  onSlideDrag(s: Slides) {
-    if (!this.slider.freeMode){
-      this.slider.freeMode = true;
-    }
-    console.log(`onSlideDrag: ${s}`);
-  }
+  // onSlideDrag(s: Slides) {
+  //   if (!this.slider.freeMode){
+  //     this.slider.freeMode = true;
+  //   }
+  //   console.log(`onSlideDrag: ${s}`);
+  // }
 
   ionViewDidLoad() {
     this.catData.getCategories(this.catId).subscribe((categories: any[]) => {
@@ -52,13 +52,50 @@ export class NewsPage {
       this.catSlides = categories.slice(0,5);
       this.categories = categories.slice(5);
     });
-    this.catListData.getCategoriesList().subscribe((categories: any[]) => {
-      this.categoriesList = categories;
-    });
+    // this.catListData.getCategoriesList().subscribe((categories: any[]) => {
+    //   this.categoriesList = categories;
+    // });
   }
 
   goToCategory(news: any) {
     this.navCtrl.push(OneDetailPage, { newsId: news.newsId });
+  }
+
+  doRefresh(refresher: any) {
+    console.log('Begin async operation', refresher);
+
+    setTimeout(() => {
+      this.catData.getCategories(8).subscribe((categories: any[]) => {
+        if (categories.length){
+          this.hasNews = true;
+        }
+        categories.map((news: any)=>{
+          news.image = news.image || 'assets/img/default/S_Topnews.png';
+          return news;
+        })
+        this.catSlides = categories.slice(0,5);
+        this.categories = categories.slice(5);
+      });
+      console.log('Async operation has ended');
+      refresher.complete();
+    }, 2000);
+  }
+
+  doInfinite(infiniteScroll: any) {
+    console.log('Begin async operation');
+
+    setTimeout(() => {
+      this.catData.getCategories(8).subscribe((categories: any[]) => {
+        categories.map((news: any)=>{
+          news.image = news.image || 'assets/img/default/S_Topnews.png';
+          this.categories.push(news);
+          return news;
+        })
+      });
+
+      console.log('Async operation has ended');
+      infiniteScroll.complete();
+    }, 500);
   }
 
 }
